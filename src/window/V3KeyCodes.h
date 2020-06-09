892e9e4962cc2be3d4bb010ac30a1af8a2c7c131
Victor Limbach <victor.limbach@epitech.eu>
2020-06-09 16:55:30 +0200

    KeyCode Aplhabet

---
+++ src/window/V3KeyCodes.h
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2020
+** V3Engine
+** File description:
+** V3KeyCodes
+*/
+
+#pragma once
+
+enum V3Keycode {
+    V3_NONE          = 0,
+    V3_A             = 4,
+    V3_B             = 5,
+    V3_C             = 6,
+    V3_D             = 7,
+    V3_E             = 8,
+    V3_F             = 9,
+    V3_G             = 10,
+    V3_H             = 11,
+    V3_I             = 12,
+    V3_J             = 13,
+    V3_K             = 14,
+    V3_L             = 15,
+    V3_M             = 16,
+    V3_N             = 17,
+    V3_O             = 18,
+    V3_P             = 19,
+    V3_Q             = 20,
+    V3_R             = 21,
+    V3_S             = 22,
+    V3_T             = 23,
+    V3_U             = 24,
+    V3_V             = 25,
+    V3_W             = 26,
+    V3_X             = 27,
+    V3_Y             = 28,
+    V3_Z             = 29,
+}
