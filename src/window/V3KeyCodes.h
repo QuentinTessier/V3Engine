2fb6fed84a36d54121360377a98a03b71cc8a08b
Victor Limbach <victor.limbach@epitech.eu>
2020-06-09 17:09:18 +0200

    Documentation

--- src/window/V3KeyCodes.h
+++ src/window/V3KeyCodes.h
@@ -8,7 +8,7 @@
 #pragma once
 
 enum V3Keycode {
-    V3_NONE          = 0,
+    V3_NONE          = 0,   // Undefined. (No event)
     V3_A             = 4,
     V3_B             = 5,
     V3_C             = 6,
@@ -35,33 +35,33 @@ enum V3Keycode {
     V3_X             = 27,
     V3_Y             = 28,
     V3_Z             = 29,
-    V3_1             = 30,
-    V3_2             = 31,
-    V3_3             = 32,
-    V3_4             = 33,
-    V3_5             = 34,
-    V3_6             = 35,
-    V3_7             = 36,
-    V3_8             = 37,
-    V3_9             = 38,
-    V3_0             = 39,
-    V3_Enter         = 40,
+    V3_1             = 30,  // 1 and !
+    V3_2             = 31,  // 2 and @
+    V3_3             = 32,  // 3 and #
+    V3_4             = 33,  // 4 and $
+    V3_5             = 34,  // 5 and %
+    V3_6             = 35,  // 6 and ^
+    V3_7             = 36,  // 7 and &
+    V3_8             = 37,  // 8 and *
+    V3_9             = 38,  // 9 and (
+    V3_0             = 39,  // 0 and )
+    V3_Enter         = 40,  // (Return)
     V3_Escape        = 41,
     V3_Delete        = 42,
     V3_Tab           = 43,
     V3_Space         = 44,
-    V3_Minus         = 45,
-    V3_Equals        = 46,
-    V3_LeftBracket   = 47,
-    V3_RightBracket  = 48,
-    V3_Backslash     = 49,
- // V3_NonUSHash     = 50,
-    V3_Semicolon     = 51,
-    V3_Quote         = 52,
+    V3_Minus         = 45,  // - and (underscore)
+    V3_Equals        = 46,  // = and +
+    V3_LeftBracket   = 47,  // [ and {
+    V3_RightBracket  = 48,  // ] and }
+    V3_Backslash     = 49,  // \ and |
+ // V3_NonUSHash     = 50,  // # and ~
+    V3_Semicolon     = 51,  // ; and :
+    V3_Quote         = 52,  // ' and "
     V3_Grave         = 53,
-    V3_Comma         = 54,
-    V3_Period        = 55,
-    V3_Slash         = 56,
+    V3_Comma         = 54,  // , and <
+    V3_Period        = 55,  // . and >
+    V3_Slash         = 56,  // / and ?
     V3_CapsLock      = 57,
     V3_F1            = 58,
     V3_F2            = 59,
@@ -84,10 +84,10 @@ enum V3Keycode {
     V3_DeleteForward = 76,
     V3_End           = 77,
     V3_PageDown      = 78,
-    V3_Right         = 79,
-    V3_Left          = 80,
-    V3_Down          = 81,
-    V3_Up            = 82,
+    V3_Right         = 79,  // Right arrow
+    V3_Left          = 80,  // Left arrow
+    V3_Down          = 81,  // Down arrow
+    V3_Up            = 82,  // Up arrow
     V3_NumLock        = 83,
     V3_Divide         = 84,
     V3_Multiply       = 85,
@@ -104,7 +104,7 @@ enum V3Keycode {
     V3_8              = 96,
     V3_9              = 97,
     V3_0              = 98,
-    V3_Point          = 99,
+    V3_Point          = 99, // . and Del
     V3_Equals         = 103,
     V3_F13           = 104,
     V3_F14           = 105,
@@ -123,12 +123,12 @@ enum V3Keycode {
     V3_Mute          = 127,
     V3_VolumeUp      = 128,
     V3_VolumeDown    = 129,
-    V3_LeftControl   = 224,
+    V3_LeftControl   = 224, // WARNING : Android has no Ctrl keys.
     V3_LeftShift     = 225,
     V3_LeftAlt       = 226,
     V3_LeftGUI       = 227,
     V3_RightControl  = 228,
-    V3_RightShift    = 229,
+    V3_RightShift    = 229, // WARNING : Win32 fails to send a WM_KEYUP message if both shift keys are pressed, and one released.
     V3_RightAlt      = 230,
     V3_RightGUI      = 231
 };
